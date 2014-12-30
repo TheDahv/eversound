5bb197bf29b22402a1f140b004fefe0b7d54285a
David Pierce <david.dean.pierce@gmail.com>
2014-12-30 15:21:46 -0800

    Introduces gulp-sass to build environment

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,13 @@
-var gulp = require('gulp'),
-jshint   = require('gulp-jshint'),
-del      = require('del'),
-path     = require('path'),
-es       = require('event-stream'),
-_        = require('underscore'),
+var gulp    = require('gulp'),
+jshint      = require('gulp-jshint'),
+del         = require('del'),
+path        = require('path'),
+es          = require('event-stream'),
+_           = require('underscore'),
+sass        = require('gulp-sass'),
 // Constants
-SCRIPTS_SRC = '_assets/js/**/*.js';
+SCRIPTS_SRC = '_assets/js/**/*.js',
+SASS_SRC    = '_assets/sass/*.scss'
 
 gulp.task('clean:scripts', function (cb) {
   return del('public/js/**', cb);
@@ -24,10 +26,22 @@ gulp.task('lint', function () {
     .pipe(jshint.reporter(require('jshint-stylish')));
 });
 
+gulp.task('sass', function () {
+  return gulp.src(SASS_SRC)
+    .pipe(sass({
+      logErrToConsole: true
+    }))
+    .pipe(gulp.dest('./public/css'));
+});
+
 gulp.task('watch:scripts', function () {
   return gulp.watch(SCRIPTS_SRC, ['lint', 'move-scripts']);
 });
 
-gulp.task('watch', ['watch:scripts'], function () {
+gulp.task('watch:sass', function () {
+  return gulp.watch(SASS_SRC, ['sass']);
+});
+
+gulp.task('watch', ['watch:scripts', 'watch:sass'], function () {
   console.log("Watching your project...");
 });
