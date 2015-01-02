30a683c4da591bd9c86d9571230e876be8ddd220
David Pierce <david.dean.pierce@gmail.com>
2015-01-02 13:12:04 -0800

    Moves broadcaster calback flow into promises

--- _assets/js/broadcaster/main.js
+++ _assets/js/broadcaster/main.js
@@ -1,6 +1,6 @@
 define(
-  ['webrtc/utils', 'webrtc/media', 'webrtc/audiovisualizer', 'webrtc/connections'],
-  function (utils, media, visualizer, connections) {
+  ['webrtc/utils', 'webrtc/media', 'webrtc/audiovisualizer', 'webrtc/connections', 'when'],
+  function (utils, media, visualizer, connections, w) {
 
   var exports = {};
   var messageName = function (channel, action) {
@@ -8,52 +8,55 @@ define(
   };
 
   exports.openChannel = function (channel) {
-    var socket = io(document.location.host), audioStream;
-
-    // Set up local audio source
-    media.initializeLocalMedia({ audio: true, video: false }).done(
-      function (stream) {
+    var socket = io(document.location.host);
+    media.initializeLocalMedia({ audio: true, video: false })
+      .then(function (stream) {
+        // Set up local audio source
         console.log("Microphone set up and waiting for speakers to connect...");
-
-        // Set library variable so peer connection handlers can
-        // access it
-        audioStream = stream;
 
         var audioContext = new AudioContext();
         var streamSource = audioContext.createMediaStreamSource(stream);
 
         visualizer.audioRings('audioRings', audioContext, streamSource);
-      },
-      function (err) {
-        console.log(err);
-      }
-    );
 
-    // Now wait for remote "speakers" to connect
-    socket.on(messageName(channel, 'description'), function (speakerDesc) {
-      console.log("Speaker joined!");
-      // Set up Peer Connection information
-      var peerConn = new RTCPeerConnection(utils.serverConfig);
+        return stream;
+      })
+      .then(function (stream) {
+        var deferred = w.defer();
 
-      // Generate broadcaster ICE candidate and send back
-      connections.handleLocalCandidate(socket, messageName(channel, 'broadcaster-ice'), peerConn);
-      connections.handleRemoteCandidate(socket, messageName(channel, 'speaker-ice'), peerConn);
+        // Now wait for remote "speakers" to connect
+        socket.on(messageName(channel, 'description'), function (speakerDesc) {
+          deferred.resolve({
+            speakerDesc: speakerDesc,
+            stream: stream
+          });
+        });
 
-      // Add audio stream to connection before responding
-      peerConn.addStream(audioStream);
+        return deferred.promise;
+      })
+      .then(function (data) {
+        console.log("Speaker joined!");
+        // Set up Peer Connection information
+        var peerConn = new RTCPeerConnection(utils.serverConfig);
 
-      connections.handleOffer(peerConn, speakerDesc)
-        .then(connections.createAnswer(socket, messageName(channel, 'description-response')))
-        .done(
-          function () {
-            console.log("Broadcaster peer connection set up complete!");
-          },
-          function (err) {
-            console.log("Broadcaster peer connection set up failed", err);
-          }
-        );
+        // Generate broadcaster ICE candidate and send back
+        connections.handleLocalCandidate(socket, messageName(channel, 'broadcaster-ice'), peerConn);
+        connections.handleRemoteCandidate(socket, messageName(channel, 'speaker-ice'), peerConn);
 
-    });
+        // Add audio stream to connection before responding
+        peerConn.addStream(data.stream);
+
+        return connections.handleOffer(peerConn, data.speakerDesc);
+      })
+      .then(connections.createAnswer(socket, messageName(channel, 'description-response')))
+      .done(
+        function () {
+          console.log("Broadcaster peer connection set up complete!");
+        },
+        function (err) {
+          console.log("Broadcaster peer connection set up failed", err);
+        }
+      );
   };
 
   return exports;
